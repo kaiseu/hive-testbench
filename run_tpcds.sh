eb44b86d4b3f29cc287b028a6d497917b57bb586
kaiseu <kaiseu@gmail.com>
2019-01-23 16:58:57 +0800

    add dataGen and load for tpc-ds

--- run_tpcds.sh
+++ run_tpcds.sh
@@ -28,6 +28,13 @@ PRINT_SETTING="${CURRENT_DIR}/sample-queries-tpcds/conf/print.settings"
 SPARKSQL_USER_CONF="${CURRENT_DIR}/sample-queries-tpcds/conf/sparksql.conf"
 QUERY_ROOT="${CURRENT_DIR}/sample-queries-tpcds"
 OUT_DIR_PATH="${CURRENT_DIR}/output"
+RAW_DATA_DIR="/tmp/tpcds-generate"
+# Tables in the TPC-DS schema.
+DIMS="date_dim time_dim item customer customer_demographics household_demographics customer_address store promotion warehouse ship_mode reason income_band call_center web_page catalog_page web_site"
+FACTS="store_sales store_returns web_sales web_returns catalog_sales catalog_returns inventory"
+################################################################################
+## DO NOT NEED TO EDIT ABOVE PARAS!!!
+################################################################################
 
 
 if test ${SCALE_FACTOR} -le 1000; then
@@ -43,9 +50,28 @@ if [[ ! -d ${OUT_DIR_PATH}/${LOG_NAME} ]];then
 	echo "Dir Created!"
 fi
 
+if [ "X$BUCKET_DATA" != "X" ]; then
+        BUCKETS=13
+        RETURN_BUCKETS=13
+else
+        BUCKETS=1
+        RETURN_BUCKETS=1
+fi
+if [ "X$DEBUG_SCRIPT" != "X" ]; then
+        set -x
+fi
+
 function usage(){
 	echo "Usage: runQuery query_number"
 	exit 1
+}
+
+function runcommand {
+        if [ "X$DEBUG_SCRIPT" != "X" ]; then
+                $1
+        else
+                $1 2>/dev/null
+        fi
 }
 
 function getExecTime() {
@@ -54,6 +80,99 @@ function getExecTime() {
 	time_s=`echo "scale=3;$(($end-$start))/1000" | bc`
 	echo "Duration: ${time_s} s"
 }
+
+function dataGen(){
+	if [ ! -f ${CURRENT_DIR}/tpcds-gen/target/tpcds-gen-1.0-SNAPSHOT.jar ]; then
+	        echo "Please build the data generator with ./tpcds-build.sh first"
+        	exit 1
+	fi
+	if [ ${SCALE_FACTOR} -eq 1 ]; then
+        	echo "Scale factor must be greater than 1"
+        	exit 1
+	fi
+
+	hdfs dfs -mkdir -p ${RAW_DATA_DIR}
+	hdfs dfs -ls ${RAW_DATA_DIR}/${SCALE_FACTOR} > /dev/null
+	if [ $? -ne 0 ]; then
+	       echo "Generating data at scale factor ${SCALE_FACTOR}."
+	        (cd tpcds-gen; hadoop jar target/*.jar -d ${RAW_DATA_DIR}/${SCALE_FACTOR}/ -s ${SCALE_FACTOR})
+	fi
+	hdfs dfs -ls ${RAW_DATA_DIR}/${SCALE_FACTOR} > /dev/null
+	if [ $? -ne 0 ]; then
+        	echo "Data generation failed, exiting."
+        	exit 1
+	fi
+
+	hadoop fs -chmod -R 777  /${RAW_DATA_DIR}/${SCALE_FACTOR}
+	echo "TPC-DS text data generation complete."
+}
+
+function populateMetastore(){
+	if [ "X${HIVE_HOME}" = "X" ]; then
+		which hive > /dev/null 2>&1
+		if [ $? -ne 0 ]; then
+        		echo "Script must be run where Hive is installed"
+        		exit 1
+		else
+			HIVE=hive
+		fi
+	else
+		HIVE="${HIVE_HOME}/bin/hive"
+	fi
+
+	# Create the partitioned and bucketed tables.
+	if [ "X${FILEFORMAT}" = "X" ]; then
+        	FILEFORMAT=orc
+	fi
+	if [ "X${DATABASE}" = "X" ]; then
+                DATABASE=tpcds_${SCHEMA_TYPE}_${FILEFORMAT}_${SCALE_FACTOR}
+        fi
+
+	LOAD_FILE="${OUT_DIR_PATH}/${LOG_NAME}/logs_tpcds_populatemetastore_${ENGINE}_${FILEFORMAT}_${SCALE_FACTOR}_`date +%Y%m%d%H%M%S`.log"
+	SILENCE="2> /dev/null 1> /dev/null"
+	if [ "X$DEBUG_SCRIPT" != "X" ]; then
+        	SILENCE=""
+	fi
+
+	echo -e "all: ${DIMS} ${FACTS}" > $LOAD_FILE
+
+	# Create the text/flat tables as external tables. These will be later be converted to ORCFile.
+	echo "Loading text data into external tables."
+	runcommand "$HIVE  -i settings/load-flat.sql -f ddl-tpcds/text/alltables.sql --hivevar DB=tpcds_text_${SCALE_FACTOR} --hivevar LOCATION=${RAW_DATA_DIR}/${SCALE_FACTOR}"
+
+	i=1
+	total=24
+	MAX_REDUCERS=2500 # maximum number of useful reducers for any scale
+	REDUCERS=$((test ${SCALE_FACTOR} -gt ${MAX_REDUCERS} && echo ${MAX_REDUCERS}) || echo ${SCALE_FACTOR})
+
+	# Populate the smaller tables.
+	for t in ${DIMS}
+	do
+        	COMMAND="$HIVE  -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
+            	--hivevar DB=${DATABASE} --hivevar SOURCE=tpcds_text_${SCALE_FACTOR} \
+            	--hivevar SCALE=${SCALE_FACTOR} \
+            	--hivevar REDUCERS=${REDUCERS} \
+            	--hivevar FILE=${FILEFORMAT}"
+        	echo -e "${t}:\n\t@$COMMAND $SILENCE && echo 'Optimizing table $t ($i/$total).'" >> $LOAD_FILE
+        	i=`expr $i + 1`
+	done
+
+	for t in ${FACTS}
+	do
+        	COMMAND="$HIVE  -i settings/load-partitioned.sql -f ddl-tpcds/bin_partitioned/${t}.sql \
+            	--hivevar DB=${DATABASE} \
+            	--hivevar SCALE=${SCALE_FACTOR} \
+            	--hivevar SOURCE=tpcds_text_${SCALE_FACTOR} --hivevar BUCKETS=${BUCKETS} \
+            	--hivevar RETURN_BUCKETS=${RETURN_BUCKETS} --hivevar REDUCERS=${REDUCERS} --hivevar FILE=${FILEFORMAT}"
+        	echo -e "${t}:\n\t@$COMMAND $SILENCE && echo 'Optimizing table $t ($i/$total).'" >> $LOAD_FILE
+        	i=`expr $i + 1`
+	done
+
+	make -j 1 -f $LOAD_FILE
+
+	echo "Data loaded into database ${DATABASE}."
+}
+
 
 ## run a single query
 function runQuery(){
