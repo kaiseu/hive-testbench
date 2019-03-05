5c677002e9cea50099f8e1641af56d1d393e9bd8
kaiseu <kai.a.xu@intel.com>
2019-03-05 11:27:01 +0800

    fixed bugs and refined for TPC-H

--- run_tpch.sh
+++ run_tpch.sh
@@ -4,7 +4,7 @@
 ## User Specific Settings, Change Based on Your Needs
 ################################################################################
 ## scale factor or data scale to run
-SCALE_FACTOR="1000"
+SCALE_FACTOR="2"
 ## engine to run, can be mr spark sparksql
 ENGINE="spark"
 ## file format, can be orc or parquet
@@ -12,7 +12,7 @@ FILEFORMAT="orc"
 ## whether to automatically clear cache before round run
 CACHE_CLEAR="true"
 ## host names used for clear cache, usually is all the machines in a cluster
-HOSTS="r73-master r73-slave1 r73-slave2 r73-slave3 r73-slave4"
+HOSTS="ubuntu16041"
 ## queries to run
 QUERY_LIST="1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22"
 
@@ -29,6 +29,7 @@ SPARKSQL_USER_CONF="${CURRENT_DIR}/sample-queries-tpch/conf/sparksql.conf"
 QUERY_ROOT="${CURRENT_DIR}/sample-queries-tpch"
 OUT_DIR_PATH="${CURRENT_DIR}/output"
 RAW_DATA_DIR="/tmp/tpch-generate"
+POPULATE_SETTING="${CURRENT_DIR}/sample-queries-tpch/conf/${ENGINE}/populate.settings"
 # Tables in the TPC-H schema.
 TABLES="part partsupp supplier customer orders lineitem nation region"
 ################################################################################
@@ -72,6 +73,20 @@ function usage(){
 #    echo "++ Duration: ${show_s}s ${show_ms}ms ++"
 #}
 #
+function DATE_PREFIX(){
+        INFO_LEVEL=$1
+        MESSAGE=$2
+        echo -e "`date '+%Y-%m-%d %H:%M:%S'` ${INFO_LEVEL}  ${MESSAGE}"
+}
+
+function runcommand {
+        if [ "X$DEBUG_SCRIPT" != "X" ]; then
+                $1
+        else
+                $1 2>/dev/null
+        fi
+}
+
 function getExecTime() {
 	start=$1
 	end=$2
@@ -89,19 +104,24 @@ function dataGen(){
                 exit 1
         fi
 
-        hdfs dfs -mkdir -p ${RAW_DATA_DIR}
-        hdfs dfs -ls ${RAW_DATA_DIR}/${SCALE_FACTOR} > /dev/null
+	hdfs dfs -ls ${RAW_DATA_DIR}/${SCALE_FACTOR} > /dev/null 2>&1
+	if [ $? == 0 ]; then
+		echo "Clearing existing data dir ${RAW_DATA_DIR}/${SCALE_FACTOR} ..."
+		hdfs dfs -rm -r -skipTrash ${RAW_DATA_DIR}/${SCALE_FACTOR} > /dev/null 2>&1
+	fi
+	#hdfs dfs -mkdir -p ${RAW_DATA_DIR}/${SCALE_FACTOR} > /dev/null
+        hdfs dfs -ls ${RAW_DATA_DIR}/${SCALE_FACTOR} > /dev/null 2>&1
         if [ $? -ne 0 ]; then
-               echo "Generating data at scale factor ${SCALE_FACTOR}."
+		echo "Generating data at scale factor ${SCALE_FACTOR}."
                 (cd tpch-gen; hadoop jar target/*.jar -d ${RAW_DATA_DIR}/${SCALE_FACTOR}/ -s ${SCALE_FACTOR})
         fi
-        hdfs dfs -ls ${RAW_DATA_DIR}/${SCALE_FACTOR} > /dev/null
+        hdfs dfs -ls ${RAW_DATA_DIR}/${SCALE_FACTOR} > /dev/null 2>&1
         if [ $? -ne 0 ]; then
                 echo "Data generation failed, exiting."
                 exit 1
         fi
 
-        hadoop fs -chmod -R 777  /${RAW_DATA_DIR}/${SCALE_FACTOR}
+        hadoop fs -chmod -R 777  ${RAW_DATA_DIR}/${SCALE_FACTOR}
         echo "TPC-H text data generation complete."
 }
 
@@ -109,7 +129,7 @@ function populateMetastore(){
         if [ "X${HIVE_HOME}" = "X" ]; then
                 which hive > /dev/null 2>&1
                 if [ $? -ne 0 ]; then
-                        echo "Script must be run where Hive is installed"
+                        DATE_PREFIX "ERROR" "Script must be run where Hive is installed"
                         exit 1
                 else
                         HIVE=hive
@@ -132,29 +152,42 @@ function populateMetastore(){
                 SILENCE=""
         fi
 
-        echo -e "all: ${TABLES}" > $LOAD_FILE
+#        echo -e "all: ${TABLES}" > $LOAD_FILE
 
         # Create the text/flat tables as external tables. These will be later be converted to ORCFile.
-        echo "Loading text data into external tables."
-        runcommand "$HIVE  -i settings/load-flat.sql -f ddl-tpch/text/alltables.sql --hivevar DB=tpch_text_${SCALE_FACTOR} --hivevar LOCATION=${RAW_DATA_DIR}/${SCALE_FACTOR}"
+	DATE_PREFIX "INFO" "Start populating metastore..." 2>&1 | tee ${LOAD_FILE} 
+        DATE_PREFIX "INFO" "Loading text data into external tables..." 2>&1 | tee -a ${LOAD_FILE}
+        runcommand "$HIVE  -i settings/load-flat.sql -f ddl-tpch/bin_flat/alltables.sql --hivevar DB=tpch_text_${SCALE_FACTOR} --hivevar LOCATION=${RAW_DATA_DIR}/${SCALE_FACTOR}" 2>&1 | tee -a ${LOAD_FILE}
         i=1
         total=8
         MAX_REDUCERS=2600 # maximum number of useful reducers for any scale
         REDUCERS=$((test ${SCALE_FACTOR} -gt ${MAX_REDUCERS} && echo ${MAX_REDUCERS}) || echo ${SCALE_FACTOR})
+	## if populate configuration file exists add it to the command
+	OPTION=()
+	if [[ -e ${POPULATE_SETTING} ]]; then
+		OPTION+=(-i ${POPULATE_SETTING})
+	fi
         # Populate the tables.
+	start=$(date +%s%3N)
         for t in ${TABLES}
         do
-                COMMAND="$HIVE  -i settings/load-${SCHEMA_TYPE}.sql -f ddl-tpch/bin_${SCHEMA_TYPE}/${t}.sql \
-                --hivevar DB=${DATABASE} --hivevar SOURCE=tpch_text_${SCALE_FACTOR} \
-                --hivevar SCALE=${SCALE_FACTOR} \
-                --hivevar REDUCERS=${REDUCERS} \
-                --hivevar FILE=${FILEFORMAT}"
-                echo -e "${t}:\n\t@$COMMAND $SILENCE && echo 'Optimizing table $t ($i/$total).'" >> $LOAD_FILE
+		DATE_PREFIX "INFO" "Populating data for table: ${t}" 2>&1 | tee -a ${LOAD_FILE}
+		
+                COMMAND="$HIVE ${OPTION[@]} -f ddl-tpch/bin_${SCHEMA_TYPE}/${t}.sql --hivevar DB=${DATABASE} --hivevar SOURCE=tpch_text_${SCALE_FACTOR} --hivevar SCALE=${SCALE_FACTOR} --hivevar REDUCERS=${REDUCERS} --hivevar FILE=${FILEFORMAT}"
+                DATE_PREFIX "INFO" "The command is: ${COMMAND}" 2>&1 | tee -a $LOAD_FILE
+		${COMMAND} 2>&1 | tee -a $LOAD_FILE
+		RES=${PIPESTATUS[0]}
+		if [[ ${RES} -ne 0 ]]; then
+			DATE_PREFIX "ERROR" "Populating data for table: ${t} FAILED! Exiting..." 2>&1 | tee -a $LOAD_FILE
+			exit -2
+		else
+			DATE_PREFIX "INFO" "Finished table: $t ($i/$total)." 2>&1 | tee -a $LOAD_FILE
+		fi
                 i=`expr $i + 1`
         done
-        make -j 1 -f $LOAD_FILE
-
-        echo "Data loaded into database ${DATABASE}."
+	end=$(date +%s%3N)
+	getExecTime $start $end 2>&1 | tee -a $LOAD_FILE
+        DATE_PREFIX "INFO" "Data loaded into database ${DATABASE}." 2>&1 | tee -a $LOAD_FILE
 }
 
 ## run a single query
@@ -164,29 +197,26 @@ function runQuery(){
 	fi
 	
 	if [[ ! -d ${OUT_DIR_PATH}/${LOG_NAME} ]];then
-        	echo "Creating output dir: ${OUT_DIR_PATH}/${LOG_NAME}"
+        	DATE_PREFIX "INFO" "Creating output dir: ${OUT_DIR_PATH}/${LOG_NAME}"
        		mkdir -p ${OUT_DIR_PATH}/${LOG_NAME}
-        	echo "Dir Created!"
+        	DATE_PREFIX "INFO" "Dir Created!"
 	fi
 	
 	OPTION=(-i ${BENCH_SETTING}) 
-	if [ -e ${GLOBAL_SETTING} ]; then
-		OPTION+=(-i ${GLOBAL_SETTING})
-	fi
 
 	LOCAL_SETTING="${CURRENT_DIR}/sample-queries-tpch/conf/${ENGINE}/tpch_query${1}_${ENGINE}.settings"
-#	if [ -e ${LOCAL_SETTING} ]; then
-#		OPTION+=(-i ${LOCAL_SETTING})
-#	fi
-	
-#	OPTION+=(-f ${QUERY_ROOT}/tpch_query${1}.sql --database ${DATABASE})
-	
-	## keep print setting at last
-       # if [ -e ${PRINT_SETTING} ]; then
-       #          OPTION+=(-i ${PRINT_SETTING})
-       # fi
 	
 	if [[ ${ENGINE} == "mr" || ${ENGINE} == "spark" ]]; then
+	        if [ -e ${GLOBAL_SETTING} ]; then
+			sed -i "/hive.execution.engine/d" ${GLOBAL_SETTING} ## delete line that contians hive.execution.engine
+			sed -i "1a\set hive.execution.engine=${ENGINE};" ${GLOBAL_SETTING} ## add set hive.execution.engine=${ENGINE}; after the 1st line
+			OPTION+=(-i ${GLOBAL_SETTING})
+	        else
+			echo "-- settings for ${ENGINE} engine" > ${GLOBAL_SETTING}
+			echo "set hive.execution.engine=${ENGINE};" >> ${GLOBAL_SETTING}
+			OPTION+=(-i ${GLOBAL_SETTING})
+	        fi
+
         	if [ -e ${LOCAL_SETTING} ]; then
                 	OPTION+=(-i ${LOCAL_SETTING})
         	fi
@@ -197,10 +227,12 @@ function runQuery(){
         	fi
 		CMD="hive ${OPTION[@]}"
 	elif [[ ${ENGINE} == "sparksql" ]]; then
-		if [ -e ${PRINT_SETTING} ]; then
+		if [ -e ${SPARKSQL_USER_CONF} ]; then
                 	OPTION+=(--properties-file ${SPARKSQL_USER_CONF})
         	fi
-		
+		if [ -e ${GLOBAL_SETTING} ]; then
+			OPTION+=(-i ${GLOBAL_SETTING})
+		fi
 		if [ -e ${LOCAL_SETTING} ]; then
                 	OPTION+=(-i ${LOCAL_SETTING})
         	fi
@@ -211,20 +243,20 @@ function runQuery(){
         	fi
 		CMD="spark-sql ${OPTION[@]}"
 	else
-		echo "Currently only support engine: mr/spark/sparksql, exiting..."
+		DATE_PREFIX "ERROR" "Currently only support engine: mr/spark/sparksql, exiting..."
 		exit -1
 	fi
 	
-	echo "Running query$1 with command: ${CMD}" 2>&1 | tee ${OUT_DIR_PATH}/${LOG_NAME}/tpch_query${1}.log
+	DATE_PREFIX "INFO" "Running query$1 with command: ${CMD}" 2>&1 | tee ${OUT_DIR_PATH}/${LOG_NAME}/tpch_query${1}.log
 	start=$(date +%s%3N)
-	eval ${CMD} 2>&1 | tee -a ${OUT_DIR_PATH}/${LOG_NAME}/tpch_query${1}.log
-	RES=$?
+	${CMD} 2>&1 | tee -a ${OUT_DIR_PATH}/${LOG_NAME}/tpch_query${1}.log
+	RES=${PIPESTATUS[0]}
 	end=$(date +%s%3N)
-	getExecTime $start $end >> ${OUT_DIR_PATH}/${LOG_NAME}/tpch_query${1}.log
+	getExecTime $start $end 2>&1 | tee -a ${OUT_DIR_PATH}/${LOG_NAME}/tpch_query${1}.log
 	if [[ ${RES} == 0 ]]; then
-		echo "query$1 finished successfully!" >> ${OUT_DIR_PATH}/${LOG_NAME}/tpch_query${1}.log
+		DATE_PREFIX "INFO" "query$1 finished successfully!" 2>&1 | tee -a ${OUT_DIR_PATH}/${LOG_NAME}/tpch_query${1}.log
 	else
-		echo "query$1 failed!" >> ${OUT_DIR_PATH}/${LOG_NAME}/tpch_query${1}.log
+		DATE_PREFIX "ERROR" "query$1 failed!" 2>&1 | tee -a ${OUT_DIR_PATH}/${LOG_NAME}/tpch_query${1}.log
 	fi
 }
 
@@ -263,14 +295,14 @@ function runAll(){
 	for ((r=1; r<=$1; r++))
 	do
 		clearCache
-		echo "Running round $r"
+		DATE_PREFIX "INFO" "Running round $r"
 		export LOG_NAME=logs_tpch_${ENGINE}_${FILEFORMAT}_${SCALE_FACTOR}_`date +%Y%m%d%H%M%S`
 		for q in ${QUERY_LIST};
 		do
 			runQuery $q
 		done
 		
-		echo "Round $r finished, logs are saved into: ${OUT_DIR_PATH}/${LOG_NAME}"
+		DATE_PREFIX "INFO" "Round $r finished, logs are saved into: ${OUT_DIR_PATH}/${LOG_NAME}"
 		## Backup the corresponding settings to log dir
 		BACKUP ${OUT_DIR_PATH}/${LOG_NAME}
 	done
@@ -283,15 +315,18 @@ function clearCache(){
                         pssh -H ${HOSTS} -t 0  -i "sync; echo 3 > /proc/sys/vm/drop_caches && printf '\n%s\n' 'Ram-cache Cleared'"
                         pssh -H ${HOSTS} -t 0  -i free -g
                 else
-                        echo "Clear cache is chosen but hosts file does not exists, will not clear cache!"
+                        DATE_PREFIX "WARNING" "Clear cache is chosen but hosts file does not exists, will not clear cache!"
                 fi
         else
-                echo "Will not automatically clear cache!"
+                DATE_PREFIX "WARNING" "Will not automatically clear cache!"
         fi
 }
 
 ################################################################################
 ## Start From Here!
 ################################################################################
+#dataGen
+populateMetastore
+#runQuery 1
 runAll 1
 #runQuery 2
